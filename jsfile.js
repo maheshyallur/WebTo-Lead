70aa881672471b08a5df8e764d29e74c088b5961
Mahesh Yallur <yallur89@gmail.com>
2023-10-20 12:57:18 +0530

    recaptchawork done

--- jsfile.js
+++ jsfile.js
@@ -1,8 +1,10 @@
- function beforeSubmit(){
-let elementOut =document.querySelector(".outputdate");
-let elementIn =document.querySelector(".inputdate");
-console.log(elementIn.value);
-let formatedDate=new Date(elementIn.value).toLocaleDateString("en-IN");
- elementOut.value=formatedDate;
- console.log(elementOut.value);
+function beforeSubmit() {
+    let elementOut = document.querySelector(".outputdate");
+    let elementIn = document.querySelector(".inputdate");
+    console.log(elementIn.value);
+    let formatedDate = new Date(elementIn.value).toLocaleDateString("en-IN");
+    elementOut.value = formatedDate;
+    console.log(elementOut.value);
 }
+
+function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") { var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value); elems["ts"] = JSON.stringify(new Date().getTime()); document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500);
